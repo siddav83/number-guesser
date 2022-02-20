3c13d866d5ab95ad13d5f0a39e7db1b18d71220c
David Siddle <siddav83@gmail.com>
2022-02-20 18:08:48 +0000

    working out the logic

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
 let SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 let recognition = new SpeechRecognition();
-
-
-
+const msgs = document.querySelector(".msg")
+const msgbox = document.querySelector(".box")
+// recognition.continuous = true;
 
 //eventListener
 
 recognition.addEventListener('result', toSpeak)
 
-
 //functions
-
-function toSpeak(event) {
-
-const msg = event.results
-  console.log(msg)
+function randomNumber() {
+  const rnd = Math.floor(Math.random() * 100)
+  return rnd
 }
 
+function writeStuff(msg) {
+  msgs.innerHTML += ` <div>You said:</div>
+  <span class="box">${msg}</span>`
+}
+
+function toSpeak(event) {
+  let msg = parseInt(event.results[0][0].transcript)
+  writeStuff(msg)
+  checkNumber(msg)
+  // checkNumber(msg)
+}
+
+function checkNumber(msg) {
+  const num = +msg
+  if(Number.isNaN(num)) {
+    console.log(num)
+    msgs.innerHTML = `
+  <div>is not a number</div>`
+  return
+}
+if(num < random) {
+  msgs.innerHTML = `<div class="box">HIGHER</div>`
+  return
+}
+else if(num > random) {
+  msgs.innerHTML = `<div class="box">LOWER</div>`
+  return
+
+}
+else if(num === random) {
+  msgs.innerHTML = `<div class="box">BINGO</div>`
+  return
+
+}
+}
 document.body.onclick = () => {
   recognition.start();
   console.log('Ready to receive a color command.');
 }
+
+const random = randomNumber()
+console.log(random)
+
+onend = () => recognition.start()
+
